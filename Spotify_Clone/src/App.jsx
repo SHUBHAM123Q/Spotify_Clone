d53970a12c08ead51410a477da0837f7a1297421
shubham__ix <shubhamrabadiya4950@gmail.com>
2024-09-11 12:26:42 +0530

    Display item compalate

--- Spotify_Clone/src/App.jsx
+++ Spotify_Clone/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Sidebar from './Components/Sidebar'
 import Player from './Components/Player'
+import Display from './Components/Display'
 
 const App = () => {
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <Sidebar />
+        <Display />
       </div>
       <Player />
     </div>
