3cff5a6044144fe0bdb008c11f6c0cd145f88f82
shubham__ix <shubhamrabadiya4950@gmail.com>
2024-09-12 11:35:36 +0530

    Album and Sonf add

--- Spotify_Clone/src/Components/Display.jsx
+++ Spotify_Clone/src/Components/Display.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useEffect, useRef } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import DisplayHome from './DisplayHome'
+import DisplayAlbum from './DisplayAlbum'
+import { albumsData } from '../assets/assets'
 
 const Display = () => {
+  const displayRef = useRef();
+  const location = useLocation();
+  // console.log(location);
+  const isAlbum = location.pathname.includes("album");
+  //  console.log(isAlbum);
+  const albumId = isAlbum ? location.pathname.slice(-1) : "";
+  // console.log(albumId);
+  const bgColor = albumsData[Number(albumId)].bgColor;
+  // console.log(bgColor);
+
+  useEffect(()=>{
+    if(isAlbum){
+      displayRef.current.style.background = `linear-gradient(${bgColor},#121212)`
+    }
+    else{
+      displayRef.current.style.background = `#121212`
+    }
+  })
+  
+  
   return (
-    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
+    <div ref={displayRef} className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
         <Routes>
             <Route path='/' element={<DisplayHome />} />
+            <Route path='/album/:id' element={<DisplayAlbum />} />
         </Routes>
     </div>
   )
